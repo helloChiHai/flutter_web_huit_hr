0a0e65d24466929b8ff4b106a193bc17dd54f7ca
helloChiHai <vuongchihai0711@gmail.com>
2025-05-07 16:48:25 +0700

    thay đổi sự kiện back ra khi hết token

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "aaef9d187591e6a8e9a5d5637b326eea",
+const RESOURCES = {"flutter_bootstrap.js": "9e8e61e759aded93301d4e86ea858afe",
 "version.json": "8e657de1dbdd0ad4853b34565605a05b",
 "index.html": "71a8fa55797cfd7bb796f5a3d719d87e",
 "/": "71a8fa55797cfd7bb796f5a3d719d87e",
-"main.dart.js": "060062890e677ccf442ae7f88b7e67dc",
+"main.dart.js": "932d39db89a6ecf3a49e07d10aad04a9",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
