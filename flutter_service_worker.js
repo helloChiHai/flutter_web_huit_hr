15a0aaacdd965faed6d64c40ec574346da5ee240
helloChiHai <vuongchihai0711@gmail.com>
2025-05-09 14:27:19 +0700

    thêm event nhận

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "09ef96fe0196411c43d1b7fe5a179e4f",
+const RESOURCES = {"flutter_bootstrap.js": "d80703ae8d500873f8280832e1113a41",
 "version.json": "8e657de1dbdd0ad4853b34565605a05b",
 "index.html": "3c9241ee8b78c46e52522f61ce443b07",
 "/": "3c9241ee8b78c46e52522f61ce443b07",
 "js/js_library.js": "b88e8a945d26d0fbdd761e2313730841",
-"main.dart.js": "69bd268ae4a5703c8455829cc19226bb",
+"main.dart.js": "75652cc70b2590be038f9cd6d4ad9cbc",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
