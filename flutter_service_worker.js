73647713ce97c8c1f60bfb988012e9b39bc3595c
helloChiHai <vuongchihai0711@gmail.com>
2025-06-03 09:40:30 +0700

    hehe

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7b123a0c3d3a9201c5bb289de36e5b71",
+const RESOURCES = {"flutter_bootstrap.js": "6e31f244cfe63c7704ad657d524b94cd",
 "version.json": "8e657de1dbdd0ad4853b34565605a05b",
 "index.html": "3c9241ee8b78c46e52522f61ce443b07",
 "/": "3c9241ee8b78c46e52522f61ce443b07",
 "js/js_library.js": "b88e8a945d26d0fbdd761e2313730841",
-"main.dart.js": "ef71b5a98de7f4591eceaedbecaa759e",
+"main.dart.js": "000a8b946a0a71c83cdb92727067f001",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
