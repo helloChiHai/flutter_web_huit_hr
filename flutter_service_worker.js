e0250e710bf60c98aaeea5141bf60fd16da52282
helloChiHai <vuongchihai0711@gmail.com>
2025-06-30 10:54:39 +0700

    đổi màu dự án quá hạn

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2a1685aa6a2f380e5f7aa96ad917b2af",
+const RESOURCES = {"flutter_bootstrap.js": "1bb90ada1cf901672fb12291bd1e6aca",
 "version.json": "8e657de1dbdd0ad4853b34565605a05b",
 "index.html": "bb1e3d3ab0ec54412a423f370b59dd0c",
 "/": "bb1e3d3ab0ec54412a423f370b59dd0c",
 "js/js_library.js": "b88e8a945d26d0fbdd761e2313730841",
 "js/face-api.min.js": "b9fa3888f6ded30c7d01a8f1ed153310",
 "js/face-api.js": "6939d12050455a60cdc7871d56e16d37",
-"main.dart.js": "04f730e4eb5dfcdf891e9699117355f0",
+"main.dart.js": "7b03ac9d49fdb4b205e76043768a985b",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "models/face_recognition_model-shard2": "f2091ed03625f6e164a637c2326691c1",
 "models/face_landmark_68_model-weights_manifest.json": "1d4029763003335bc6921aadeb58706a",
