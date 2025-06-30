211934fae7329e1d0d514e1ad782bf07405a8509
helloChiHai <vuongchihai0711@gmail.com>
2025-06-30 09:34:11 +0700

    fix mau home

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "db9a88c32f9208b8eeace92afb499946",
+const RESOURCES = {"flutter_bootstrap.js": "ed8bcfa244b69b0d90ee7bd1b0490407",
 "version.json": "8e657de1dbdd0ad4853b34565605a05b",
 "index.html": "bb1e3d3ab0ec54412a423f370b59dd0c",
 "/": "bb1e3d3ab0ec54412a423f370b59dd0c",
 "js/js_library.js": "b88e8a945d26d0fbdd761e2313730841",
 "js/face-api.min.js": "b9fa3888f6ded30c7d01a8f1ed153310",
 "js/face-api.js": "6939d12050455a60cdc7871d56e16d37",
-"main.dart.js": "fb447781a68c647b5279163e080fcb28",
+"main.dart.js": "012763b8d5d16efcd03918159d754f35",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "models/face_recognition_model-shard2": "f2091ed03625f6e164a637c2326691c1",
 "models/face_landmark_68_model-weights_manifest.json": "1d4029763003335bc6921aadeb58706a",
