ec46256afbb70ff03bf79d3f62996222230de1a9
helloChiHai <vuongchihai0711@gmail.com>
2025-07-11 15:01:55 +0700

    check nhận color từ home

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "388242793"
+    serviceWorkerVersion: "967270311"
   }
 });
